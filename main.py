8bc1c8b20d000ac73e46e20fb099e2ef8536bd6d
Mitchell Carlson <mitchell5433@gmail.com>
2024-07-23 21:24:50 -0500

    Format with Black Formatter

--- main.py
+++ main.py
@@ -6,22 +6,31 @@ from datetime import datetime
 from os import access, R_OK
 from os.path import isfile
 from typing import Dict
-from renogybt import InverterClient, RoverClient, RoverHistoryClient, BatteryClient, DataLogger, Utils
+from renogybt import (
+    InverterClient,
+    RoverClient,
+    RoverHistoryClient,
+    BatteryClient,
+    DataLogger,
+    Utils,
+)
 
 
 def is_readable(file):
     return isfile(file) and access(file, R_OK)
 
+
 def load_user_config():
     try:
-        with open('/data/options.json') as f:
+        with open("/data/options.json") as f:
             conf = dotdict(json.load(f))
         return conf
     except Exception as e:
-        print('error reading /data/options.json, trying options.json', e)
-        with open('options.json') as f:
+        print("error reading /data/options.json, trying options.json", e)
+        with open("options.json") as f:
             conf = dotdict(json.load(f))
         return conf
+
 
 class dotdict(dict):
     def __getattr__(self, attr):
@@ -32,6 +41,7 @@ class dotdict(dict):
 
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
+
 
 # Load config
 json_config: Dict[str, any] = load_user_config()
@@ -52,37 +62,44 @@ for section, options in json_config.items():
 # Set up remote logging
 data_logger: DataLogger = DataLogger(configparser_config)
 
+
 # The callback function when data is received
 async def on_data_received(client, data):
-    filtered_data = Utils.filter_fields(data, configparser_config['data']['fields'])
+    filtered_data = Utils.filter_fields(data, configparser_config["data"]["fields"])
     logging.info(f"{client.bleManager.device.name} => {filtered_data}")
-    if configparser_config['remote_logging'].getboolean('enabled'):
+    if configparser_config["remote_logging"].getboolean("enabled"):
         await data_logger.log_remote(json_data=filtered_data)
-    if configparser_config['mqtt'].getboolean('enabled'):
+    if configparser_config["mqtt"].getboolean("enabled"):
         await data_logger.log_mqtt(json_data=filtered_data)
-    if configparser_config['pvoutput'].getboolean('enabled') and configparser_config['device']['type'] == 'RNG_CTRL':
+    if (
+        configparser_config["pvoutput"].getboolean("enabled")
+        and configparser_config["device"]["type"] == "RNG_CTRL"
+    ):
         await data_logger.log_pvoutput(json_data=filtered_data)
     await client.stop()
+
 
 # Start client
 # TODO: Convert to run with multiple devices
 async def start_client():
     logger.info(f"Device type: {configparser_config['device']['type']}")
-    if configparser_config['device']['type'] == 'RNG_CTRL':
+    if configparser_config["device"]["type"] == "RNG_CTRL":
         await RoverClient(configparser_config, on_data_received).start()
-    elif configparser_config['device']['type'] == 'RNG_CTRL_HIST':
+    elif configparser_config["device"]["type"] == "RNG_CTRL_HIST":
         await RoverHistoryClient(configparser_config, on_data_received).start()
-    elif configparser_config['device']['type'] == 'RNG_BATT':
+    elif configparser_config["device"]["type"] == "RNG_BATT":
         await BatteryClient(configparser_config, on_data_received).start()
-    elif configparser_config['device']['type'] == 'RNG_INVT':
+    elif configparser_config["device"]["type"] == "RNG_INVT":
         await InverterClient(configparser_config, on_data_received).start()
     else:
         logging.error("unknown device type")
+
 
 async def main():
     while True:
         await start_client()
         await asyncio.sleep(configparser_config["data"].getint("poll_interval"))
+
 
 if __name__ == "__main__":
     asyncio.run(main())
--- renogybt/BaseClient.py
+++ renogybt/BaseClient.py
@@ -8,10 +8,11 @@ from .Utils import bytes_to_int, crc16_modbus, int_to_bytes
 # Should be extended by each client with its own parsers and section definitions
 # Section example: {'register': 5000, 'words': 8, 'parser': self.parser_func}
 
-ALIAS_PREFIX = 'BT-TH'
+ALIAS_PREFIX = "BT-TH"
 NOTIFY_CHAR_UUID = "0000fff1-0000-1000-8000-00805f9b34fb"
 WRITE_CHAR_UUID = "0000ffd1-0000-1000-8000-00805f9b34fb"
 READ_TIMEOUT = 15  # (seconds)
+
 
 class BaseClient:
     def __init__(self, config):
@@ -21,11 +22,13 @@ class BaseClient:
         self.poll_timer = None
         self.read_timeout = None
         self.data = {}
-        self.device_id = self.config['device'].getint('device_id')
+        self.device_id = self.config["device"].getint("device_id")
         self.sections = []
         self.section_index = 0
         self.loop = asyncio.get_event_loop()
-        logging.info(f"Init {self.__class__.__name__}: {self.config['device']['alias']} => {self.config['device']['mac_addr']}")
+        logging.info(
+            f"Init {self.__class__.__name__}: {self.config['device']['alias']} => {self.config['device']['mac_addr']}"
+        )
 
     async def start(self):
         try:
@@ -37,20 +40,24 @@ class BaseClient:
 
     async def connect(self):
         self.bleManager = BLEManager(
-            mac_address=self.config['device']['mac_addr'],
-            alias=self.config['device']['alias'],
+            mac_address=self.config["device"]["mac_addr"],
+            alias=self.config["device"]["alias"],
             on_data=self.on_data_received,
             on_connect_fail=self.__on_connect_fail,
             notify_uuid=NOTIFY_CHAR_UUID,
-            write_uuid=WRITE_CHAR_UUID
+            write_uuid=WRITE_CHAR_UUID,
         )
         await self.bleManager.discover()
 
         if not self.bleManager.device:
-            logging.error(f"Device not found: {self.config['device']['alias']} => {self.config['device']['mac_addr']}, please check the details provided.")
+            logging.error(
+                f"Device not found: {self.config['device']['alias']} => {self.config['device']['mac_addr']}, please check the details provided."
+            )
             for dev in self.bleManager.discovered_devices:
                 if dev.name and dev.name.startswith(ALIAS_PREFIX):
-                    logging.info(f"Possible device found! ====> {dev.name} > [{dev.address}]")
+                    logging.info(
+                        f"Possible device found! ====> {dev.name} > [{dev.address}]"
+                    )
             await self.stop()
         else:
             await self.bleManager.connect()
@@ -67,13 +74,17 @@ class BaseClient:
 
         if operation == 3:  # read operation
             logging.info(f"on_data_received: response for read operation")
-            if (self.section_index < len(self.sections) and
-                self.sections[self.section_index]['parser'] and
-                self.sections[self.section_index]['words'] * 2 + 5 == len(response)):
+            if (
+                self.section_index < len(self.sections)
+                and self.sections[self.section_index]["parser"]
+                and self.sections[self.section_index]["words"] * 2 + 5 == len(response)
+            ):
                 # parse and update data
-                self.sections[self.section_index]['parser'](response)
+                self.sections[self.section_index]["parser"](response)
 
-            if self.section_index >= len(self.sections) - 1:  # last section, read complete
+            if (
+                self.section_index >= len(self.sections) - 1
+            ):  # last section, read complete
                 self.section_index = 0
                 self.on_read_operation_complete()
                 self.data = {}
@@ -87,8 +98,8 @@ class BaseClient:
 
     def on_read_operation_complete(self):
         logging.info("on_read_operation_complete")
-        self.data['__device'] = self.config['device']['alias']
-        self.data['__client'] = self.__class__.__name__
+        self.data["__device"] = self.config["device"]["alias"]
+        self.data["__client"] = self.__class__.__name__
         if self.on_data_callback:
             asyncio.create_task(self.on_data_callback(self, self.data))
 
@@ -97,8 +108,8 @@ class BaseClient:
         asyncio.create_task(self.stop())
 
     async def check_polling(self):
-        if self.config['data'].getboolean('enable_polling'):
-            await asyncio.sleep(self.config['data'].getint('poll_interval'))
+        if self.config["data"].getboolean("enable_polling"):
+            await asyncio.sleep(self.config["data"].getint("poll_interval"))
             await self.read_section()
 
     async def read_section(self):
@@ -107,7 +118,12 @@ class BaseClient:
             return logging.error("BaseClient cannot be used directly")
 
         self.read_timeout = self.loop.call_later(READ_TIMEOUT, self.on_read_timeout)
-        request = self.create_generic_read_request(self.device_id, 3, self.sections[index]['register'], self.sections[index]['words'])
+        request = self.create_generic_read_request(
+            self.device_id,
+            3,
+            self.sections[index]["register"],
+            self.sections[index]["words"],
+        )
         await self.bleManager.characteristic_write_value(request)
 
     def create_generic_read_request(self, device_id, function, regAddr, readWrd):
